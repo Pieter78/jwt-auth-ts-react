c1304df800d49bc73fcbd71a16d67a6533d6c401
Pieter Overbeek <pieter@belfabriek.nl>
2019-09-23 10:19:00 +0300

    Setup express/apollo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,44 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import 'reflect-metadata'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+;(async () => {
+  const app = express()
+  app.get('/', (_, res) => res.send('hello'))
 
-createConnection().then(async connection => {
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+        type Query {
+            hello: String!
+        } 
+      `,
+    resolvers: {
+      Query: {
+        hello: () => 'hello world',
+      },
+    },
+  })
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+  apolloServer.applyMiddleware({ app })
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+  app.listen(4000, () => {
+    console.log('server started on port 4000')
+  })
+})()
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+// createConnection()
+//   .then(async connection => {
+//     console.log('Inserting a new user into the database...')
+//     const user = new User()
+//     user.firstName = 'Timber'
+//     user.lastName = 'Saw'
+//     user.age = 25
+//     await connection.manager.save(user)
+//     console.log('Saved a new user with id: ' + user.id)
 
-}).catch(error => console.log(error));
+//     console.log('Loading users from the database...')
+//     const users = await connection.manager.find(User)
+//     console.log('Loaded users: ', users)
+
+//     console.log('Here you can setup and run express/koa/any other framework.')
+//   })
+//   .catch(error => console.log(error))
